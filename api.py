f396df99a1068e36210b1644feefc458fd815e64
Floria Gu <gfzting@gmail.com>
2022-06-30 11:33:08 -0700

    Add simple (non thread safe) connection pool

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from flask import request
 from dotenv import load_dotenv
 import os
 import psycopg2
+from psycopg2 import pool
 
 load_dotenv()
 
@@ -12,134 +13,143 @@ DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 
 app = Flask(__name__)
 
-try:
-    conn = psycopg2.connect(
-        database=DATABASE,
-        user=DATABASE_USERNAME,
-        password=DATABASE_PASSWORD
-    )
+postgreSQL_pool = psycopg2.pool.SimpleConnectionPool(1, 10, database=DATABASE,
+                                                     user=DATABASE_USERNAME,
+                                                     password=DATABASE_PASSWORD)
 
+
+@app.route("/api/v1/resources/users", methods=["POST"])
+def add_user():
+    conn = postgreSQL_pool.getconn()
     cursor = conn.cursor()
 
+    body = request.json
+    if validate_user_data(body):
+        user = body["user"]
+    else:
+        return {"error": {"msg": "Input is invalid."}}, 400
 
-    @app.route("/api/v1/resources/users", methods=["POST"])
-    def add_user():
-        body = request.json
-        if validate_user_data(body):
-            user = body["user"]
-        else:
-            return {"error": {"msg": "Input is invalid."}}, 400
+    try:
+        cursor.execute("""
+                        INSERT INTO users (email, password, first_name, last_name)
+                        VALUES (%s, %s, %s, %s)
+                        RETURNING user_id, email, password, first_name, last_name;
+                        """,
+                       (user["email"], user["password"], user["firstName"], user["lastName"]))
+        conn.commit()
+    except psycopg2.Error as error:
+        return {"error": {"msg": str(error)}}, 400
 
-        try:
-            cursor.execute("""
-                            INSERT INTO users (email, password, first_name, last_name)
-                            VALUES (%s, %s, %s, %s)
-                            RETURNING user_id, email, password, first_name, last_name;
-                            """,
-                           (user["email"], user["password"], user["firstName"], user["lastName"]))
-            conn.commit()
-        except psycopg2.Error as error:
-            return {"error": {"msg": str(error)}}, 400
+    result = cursor.fetchone()
 
-        result = cursor.fetchone()
+    cursor.close()
+    postgreSQL_pool.putconn(conn)
 
-        return {
-            "result": {
-                "id": result[0],
-                "email": result[1],
-                "password": result[2],
-                "firstName": result[3],
-                "lastName": result[4],
-            }
+    return {
+        "result": {
+            "id": result[0],
+            "email": result[1],
+            "password": result[2],
+            "firstName": result[3],
+            "lastName": result[4],
         }
+    }
 
 
-    def validate_user_data(user_data) -> bool:
-        if type(user_data) is not dict:
+def validate_user_data(user_data) -> bool:
+    if type(user_data) is not dict:
+        return False
+
+    user = user_data.get("user")
+    if not user:
+        return False
+
+    required_fields = ["email", "password", "firstName", "lastName"]
+    for field in required_fields:
+        field_value = user.get(field)
+        if not field_value or type(field_value) is not str:
             return False
 
-        user = user_data.get("user")
-        if not user:
-            return False
-
-        required_fields = ["email", "password", "firstName", "lastName"]
-        for field in required_fields:
-            field_value = user.get(field)
-            if not field_value or type(field_value) is not str:
-                return False
-
-        return True
+    return True
 
 
-    @app.route("/api/v1/resources/users/<user_id>", methods=["GET"])
-    def get_user(user_id):
-        try:
-            user_id = int(user_id)
-        except ValueError:
-            return {"error": {"msg": "User id is invalid."}}, 400
+@app.route("/api/v1/resources/users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        return {"error": {"msg": "User id is invalid."}}, 400
 
-        try:
-            cursor.execute("""
-                            SELECT user_id, email, first_name, last_name
-                            FROM users
-                            WHERE user_id = %s;
-                            """,
-                           (user_id,))
+    conn = postgreSQL_pool.getconn()
+    cursor = conn.cursor()
 
-            user = cursor.fetchone()
-        except psycopg2.Error as error:
-            return {"error": {"msg": str(error)}}, 400
+    try:
+        cursor.execute("""
+                        SELECT user_id, email, first_name, last_name
+                        FROM users
+                        WHERE user_id = %s;
+                        """,
+                       (user_id,))
 
-        if user:
-            return {
-                "result": {
-                    "id": user[0],
-                    "email": user[1],
-                    "firstName": user[2],
-                    "lastName": user[3],
-                }
-            }
-        else:
-            return {"error": {"msg": f"Could not find a user with ID {user_id}."}}, 400
+        user = cursor.fetchone()
+    except psycopg2.Error as error:
+        return {"error": {"msg": str(error)}}, 400
 
+    cursor.close()
+    postgreSQL_pool.putconn(conn)
 
-    @app.route("/api/v1/resources/users/<user_id>", methods=["PUT"])
-    def update_user(user_id):
-        try:
-            user_id = int(user_id)
-        except ValueError:
-            return {"error": {"msg": "User id is invalid."}}, 400
-
-        body = request.json
-        if validate_user_data(body):
-            user = body["user"]
-        else:
-            return {"error": {"msg": "Input is invalid."}}, 400
-
-        try:
-            cursor.execute("""
-                            UPDATE users
-                            SET email = %s, password = %s, first_name = %s, last_name = %s
-                            WHERE user_id = %s
-                            RETURNING user_id, email, password, first_name, last_name;
-                            """,
-                           (user["email"], user["password"], user["firstName"], user["lastName"], user_id))
-            conn.commit()
-        except psycopg2.Error as error:
-            return {"error": {"msg": str(error)}}, 400
-
-        result = cursor.fetchone()
-
+    if user:
         return {
             "result": {
-                "id": result[0],
-                "email": result[1],
-                "password": result[2],
-                "firstName": result[3],
-                "lastName": result[4],
+                "id": user[0],
+                "email": user[1],
+                "firstName": user[2],
+                "lastName": user[3],
             }
         }
+    else:
+        return {"error": {"msg": f"Could not find a user with ID {user_id}."}}, 400
 
 
-except BaseException as err:
-    print(err)
+@app.route("/api/v1/resources/users/<user_id>", methods=["PUT"])
+def update_user(user_id):
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        return {"error": {"msg": "User id is invalid."}}, 400
+
+    conn = postgreSQL_pool.getconn()
+    cursor = conn.cursor()
+
+    body = request.json
+    if validate_user_data(body):
+        user = body["user"]
+    else:
+        return {"error": {"msg": "Input is invalid."}}, 400
+
+    try:
+        cursor.execute("""
+                        UPDATE users
+                        SET email = %s, password = %s, first_name = %s, last_name = %s
+                        WHERE user_id = %s
+                        RETURNING user_id, email, password, first_name, last_name;
+                        """,
+                       (user["email"], user["password"], user["firstName"], user["lastName"], user_id))
+        conn.commit()
+    except psycopg2.Error as error:
+        return {"error": {"msg": str(error)}}, 400
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    postgreSQL_pool.putconn(conn)
+
+    return {
+        "result": {
+            "id": result[0],
+            "email": result[1],
+            "password": result[2],
+            "firstName": result[3],
+            "lastName": result[4],
+        }
+    }
